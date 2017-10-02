6aaed40eaf07cab27cc0ba00ae148755c9aed7b8
James Pizagno <jamespizagno@Jamess-MacBook-Pro.local>
2017-10-02 20:13:47 +0200

    fixed index.js for Docker and build.sh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import ReactDOM from 'react-dom';
 import Modal from 'react-responsive-modal';
 import './css/index.css';
