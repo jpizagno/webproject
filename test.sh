95dd4afaac6b496e29557b88aad0f461c24b4b2f
EC2 Default User <ec2-user@ip-172-31-21-54.eu-central-1.compute.internal>
2017-10-13 17:19:09 +0000

    edited test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 
 # npm install
+sudo yum install nodejs npm --enablerepo=epel -y
 sudo yum install npm  -y
-npm install react-responsive-modal --save 
-npm install -g create-react-app
+sudo npm install n -g
+sudo n stable
+sudo npm install react-responsive-modal --save 
+sudo npm install -g create-react-app
 
-npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
-npm install --save-dev enzyme
-npm i --save-dev react-test-renderer@16
-npm i --save-dev enzyme enzyme-adapter-react-16
+sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
+# sudo npm install --save-dev enzyme
+sudo npm i --save-dev react-test-renderer@16
+# sudo npm i --save-dev enzyme enzyme-adapter-react-16
 
 # create test project
 create-react-app my-app-test
