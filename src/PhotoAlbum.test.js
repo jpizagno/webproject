3a30e704fc52ec0ae1aa6598eaa5d21d6512099e
EC2 Default User <ec2-user@ip-172-31-18-122.eu-central-1.compute.internal>
2017-12-22 10:20:36 +0000

    edited tests

--- src/PhotoAlbum.test.js
+++ src/PhotoAlbum.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import ReactDOM from 'react-dom';
 import PhotoAlbum from './PhotoAlbum.js';
-import Adapter from 'enzyme-adapter-react-16';
 
-import { configure } from 'enzyme';
-
-configure({ adapter: new Adapter() });
-
-it('Integration Test:  renders without crashing', () => {
-  const photoAlbum = shallow ( <PhotoAlbum /> );
-
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<PhotoAlbum />, div);
 });
--- test.sh
+++ test.sh
@@ -10,10 +10,10 @@ sudo n stable
 sudo npm install react-responsive-modal --save 
 sudo npm install -g create-react-app
 
-sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
-sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
+# sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
+# sudo npm install --save-dev jest babel-jest babel-preset-es2015 babel-preset-react react-test-renderer
 # sudo npm install --save-dev enzyme
-sudo npm i --save-dev react-test-renderer@16
+# sudo npm i --save-dev react-test-renderer@16
 # sudo npm i --save-dev enzyme enzyme-adapter-react-16
 
 # create test project
@@ -23,9 +23,16 @@ create-react-app my-app-test
 
 # copy software into my-app-test
 rm -rf my-app-test/src/*
-cp -R * my-app-test/
+# cp -R * my-app-test/
+rsync -av ./src/ ./my-app-test/src/ 
 
 # run tests
+echo '   '
+echo ' ************** '
+echo ' cd into my-app-test/ '
 cd my-app-test/
+echo ' ************* '
+echo ' running npm install '
 npm install
+echo ' running npm test '
 npm test
